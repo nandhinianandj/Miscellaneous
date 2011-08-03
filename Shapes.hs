3a8dd764f7939fc7164c6fc7c8c322c0f4194cf1
Software Mechanic <softwaremechanic32@gmail.com>
2011-08-03 14:17:13 +0530

    Just a few fixes...

--- Shapes.hs
+++ Shapes.hs
@@ -7,6 +7,8 @@ module Shapes
 , baseRect
 ) where
 
+import qualified Data.Map as Map
+
 data Point = Point Float Float deriving (Show)
 data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
 
@@ -27,11 +29,11 @@ data Vector a = Vector a a a deriving (Show)
 
 surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
  
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
-nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) Point (x2 + b))
+nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
 
 baseCircle :: Float -> Shape
 baseCircle r = Circle (Point 0 0) r 
@@ -50,3 +52,102 @@ vectMult :: (Num t) => Vector t -> t -> Vector t
 
 scalarMult :: (Num t) => Vector t -> Vector t -> t
 (Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
+
+data Day =  Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
+            deriving (Eq, Ord, Show, Read, Bounded, Enum)
+
+--type String = [Char]
+
+--type PhoneBook = [(String, String)]
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name,PhoneNumber)]
+
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+type AssocList k v = [(k,v)]
+
+type IntMap = Map Int
+
+data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)
+
+
+data LockerState = Taken | Free deriving (Show, Eq)
+
+type Code = String
+
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either String Code
+lockerLookup lockerNumber map =
+    case Map.lookup lockerNumber map of
+        Nothing -> Left $ "Locker number" ++ show lockerNumber ++ "doesn't exist!"
+        Just (state, code) -> if state /= Taken
+                                then Right code
+                                else Left $ "Locker" ++ show lockerNumber ++ "isalready taken!"
+
+lockers :: LockerMap
+lockers = Map.fromList
+    [(100,(Taken, "ZD391"))
+    ,(101,(Free, "JAH3I"))
+    ,(103,(Free, "IQSA9"))
+    ,(105,(Free, "QOTSA"))
+    ,(109,(Taken, "893JJ"))
+    ,(110,(Taken, "99292"))
+    ]
+
+--data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 ++
+(++) :: [a] -> [a] -> [a]
+[]      ++ ys = ys
+(x:xs)  ++ ys = x : (xs ++ ys)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+    | x == a = True
+    | x < a  = treeElem x left
+    | x > a  = treeElem x right
+
+
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+    Red == Red == True
+    Green == Green == True
+    Yellow == Yellow == True
+    _ == _ = False
+
+instance Show TrafficLight where
+    show Red = "Red Light"
+    show Yellow = "Yellow Light"
+    show Green = "Green Light"
+
+
+
--- xmonad.hs
+++ xmonad.hs
@@ -1,12 +1,14 @@
 import XMonad
-import qualified XMonad.StackSet as W
+import qualified XMonad.StackSet as S
 import qualified Data.Map        as M
 import Control.Monad
 import System.IO
 import System.IO.Unsafe
 import Data.List
 import Data.Ratio ((%))
- 
+
+import Xmonad.Config.Gnome
+
 -- Actions
 import XMonad.Actions.CycleWS
 import XMonad.Actions.Plane
@@ -14,6 +16,7 @@ import XMonad.Actions.TopicSpace
 import XMonad.Actions.SpawnOn
 import XMonad.Actions.OnScreen
 import XMonad.Actions.SwapWorkspaces
+import XMonad.Actions.FindEmptyWorkspace
  
 -- Hooks
 import XMonad.Hooks.ManageHelpers
@@ -30,6 +33,9 @@ import XMonad.Layout.Grid
 import XMonad.Layout.TwoPane
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Reflect
+import XMonad.Layout.Named
+import XMonad.Layout.LayoutModifier
+
  
 -- Prompts
 import XMonad.Prompt
@@ -43,7 +49,14 @@ import XMonad.Prompt.AppendFile
 -- Util
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP, additionalKeys)
- 
+
+
+-- | Data type for LayoutModifier which converts given layout to IM-layout
+-- (with dedicated space for the roster and original layout for chat windows)
+data AddRosters a = AddRosters Rational [Property] deriving (Read, Show) 
+
+
+myBaseConfig = gnomeConfig
 --
 -- main
 --
@@ -68,18 +81,65 @@ main = do
 	} `additionalKeysP` myKeys
  
 -- End Main
- 
+
+
+-- | Modifier which converts given layout to IMs-layout (with dedicated
+-- space for rosters and original layout for chat windows)
+withIMs :: LayoutClass l a => Rational -> [Property] -> l a -> ModifiedLayout AddRosters l a
+withIMs ratio props = ModifiedLayout $ AddRosters ratio props 
+
+-- | IM layout modifier applied to the Grid layout
+gridIMs :: Rational -> [Property] -> ModifiedLayout AddRosters Grid a
+gridIMs ratio props = withIMs ratio props Grid
+
 -- Simple configuration
 myBorderWidth = 2
 myBrowser = "firefox"
 myTerminal = "gnome-terminal --window-with-profile=xmonad"
 myShell = "zsh"
-myModMask = mod4Mask
 myIconDir = "/home/anand/.dzen/dzenIcons/"
 myStatusBar = "dzen2 -x '0' -y '0' -h '20' -w '330' -ta 'l' -bg '" ++ myDBGColor ++ "' -fn '" ++ myFont ++ "'"
 myLeft = "/home/anand/.dzen/left.zsh | dzen2 -xs 1 -x '330' -y '0' -h '20' -w '1110' -ta 'r' -bg '" ++ myDBGColor ++ "' -fg '" ++ myDFGColor ++ "' -fn '" ++ myFont ++ "'"
 myRight = "/home/anand/.dzen/right.zsh | dzen2 -xs 2 -y '0' -h '20' -ta 'r' -bg '" ++ myDBGColor ++ "' -fg '" ++ myDFGColor ++ "' -fn '" ++ myFont ++ "'"
 myFont = "DroidSans-Bold.ttf" 
+
+-- workspaces
+myWorkspaces = ["web", "editor", "terms"] ++ (miscs 5) ++ ["fullscreen", "im"]
+    where miscs = map (("misc" ++) . show) . (flip take) [1..]
+isFullscreen = (== "fullscreen")
+-- layouts
+basicLayout = Tall nmaster delta ratio where
+    nmaster = 1
+    delta   = 3/100
+    ratio   = 1/2
+tallLayout = named "tall" $ avoidStruts $ basicLayout
+wideLayout = named "wide" $ avoidStruts $ Mirror basicLayout
+singleLayout = named "single" $ avoidStruts $ noBorders Full
+fullscreenLayout = named "fullscreen" $ noBorders Full
+imLayout = avoidStruts $ reflectHoriz $ withIMs ratio rosters chatLayout where
+    chatLayout      = Grid
+    ratio           = 1%6
+    rosters         = [skypeRoster, pidginRoster]
+    pidginRoster    = And (ClassName "Pidgin") (Role "buddy_list")
+    skypeRoster     = (ClassName "Skype") `And` (Not (Title "Options")) `And` (Not (Role "Chats")) `And` (Not (Role "CallWindowForm"))
+--                                       
+myLayoutHook = fullscreen $ im $ normal where
+    normal     = tallLayout ||| wideLayout ||| singleLayout
+    fullscreen = onWorkspace "fullscreen" fullscreenLayout
+    im         = onWorkspace "im" imLayout
+--                                                    
+-- special treatment for specific windows:
+-- put the Pidgin and Skype windows in the im workspace
+myManageHook = imManageHooks <+> manageHook myBaseConfig
+imManageHooks = composeAll [isIM --> moveToIM] where
+    isIM     = foldr1 (<||>) [isPidgin, isSkype]
+    isPidgin = className =? "Pidgin"
+    isSkype  = className =? "Skype"
+    moveToIM = doF $ S.shift "im"
+--                                                                     
+-- Mod4 is the Super / Windows key
+myModMask = mod4Mask
+altMask = mod1Mask
  
 -- Layout Hook
 mylayoutHook = smartBorders $ avoidStruts $ lessBorders (Combine Difference Screen OnlyFloat) (Mirror tiled ||| tiled ||| fullscreenLayout)
@@ -89,21 +149,6 @@ mylayoutHook = smartBorders $ avoidStruts $ lessBorders (Combine Difference Scre
 	nmaster = 1
 	delta = 3 / 100
 	ratio = 11 / 20
- 
--- Manage hook
-myManageHook = composeAll
-    [  className =? "Xmessage"  --> doFloat 
-    ,  className =? "Pidgin"    --> doShift "im"      
-    ,  className =? "Navigator" --> doShift "web"      
-    ,  title	 =? "wowee"     --> doShift "term"
-    ,  title	 =? "dev"       --> doShift "dev"
-    ,  title     =? "irssi"     --> doShift "irc"
-    ,  title     =? "nyxmms2"   --> doShift "music"
-    ,  title     =? "sup"       --> doShift "mail"
-    ,  title     =? "htop"      --> doShift "mon"
-    ] 
-		<+> (fmap not isDialog --> doF avoidMaster)
-		<+> composeOne [ isFullscreen -?> doFullFloat ]
  
 -- Log Hook
 myLogHook = fadeInactiveLogHook fadeAmount
@@ -148,41 +193,78 @@ myDzenPP h = defaultPP
                             )
       , ppSep             = " "
       , ppOutput          = hPutStrLn h }
+
+
+
+-- better Key bindings for dvorak
+myKeys conf = M.fromList $
+    [ ((myModMask              ,xK_Return), spawn $ Xmonad.terminal conf)
+    , ((myModMask              , xK_r     ), gnomeRun)
+    , ((myModMask              , xK_c     ), kill)
+    , ((myModMask              , xK_space ), sendMessage NextLayout)
+    , ((myModMask              , xK_n     ), refresh)
+    , ((myModMask              , xK_m     ), windows S.swapMaster)
+    , ((altMask                , xK_Tab   ), windows S.focusDown)
+    , ((altMask .|. shiftMask  , xK_Tab   ), windows S.focusUp)
+    , ((myModMask              , xK_Down  ), windows S.swapDown)
+    , ((myModMask              , xK_Up    ), windows S.swapUp)
+    , ((myModMask              , xK_Left  ), sendMessage Shrink)
+    , ((myModMask              , xK_Right ), sendMessage Expand)
+    , ((myModMask              , xK_t     ), withFocused $ windows . S.sink)
+    , ((myModMask              , xK_w     ), sendMessage (IncMasterN 1))
+    , ((myModMask              , xK_v     ), sendMessage (IncMasterN (-1)))
+    , ((myModMask              , xK_q     ), broadcastMessage ReleaseResources >> restart "xmonad" True)
+    , ((myModMask .|. shiftMask, xK_q     ), spawn "gnome-session-save --kill")
+    , ((altMask .|. controlMask, xK_Left  ), prevWS)
+    , ((altMask .|. controlMask, xK_Right ), nextWS)
+    ] ++
+    -- Alt+F1..F10 switches to workspace
+    -- (Alt is in a nicer location for the thumb than the Windows key,
+    -- and 1..9 keys are already in use by Firefox, irssi, ...)
+    [ ((altMask, k), windows $ S.greedyView i)
+        | (i, k) <- zip myWorkspaces workspaceKeys
+    ] ++
+    -- mod+F1..F10 moves window to workspace and switches to that workspace
+    [ ((myModMask, k), (windows $ S.shift i) >> (windows $ S.greedyView i))
+        | (i, k) <- zip myWorkspaces workspaceKeys
+    ]
+    where workspaceKeys = [xK_F1 .. xK_F10]
  
 -- Key Bindings 
-myKeys = [ ("M-f",	spawn myBrowser)  	
+--myKeys = [ ("M-f",	spawn myBrowser)  	
          -- xmms2
-         , ("M-S-'",	spawn "nyxmms2 prev")
-         , ("M-S-.",	spawn "nyxmms2 next")
-         , ("M-S-p",	spawn "nyxmms2 toggle")
+--         , ("M-S-'",	spawn "nyxmms2 prev")
+--         , ("M-S-.",	spawn "nyxmms2 next")
+--         , ("M-S-p",	spawn "nyxmms2 toggle")
 	 -- ALSA Volume
-         , ("M-C-e",	spawn "amixer set Master 2%-")
-         , ("M-C-u",	spawn "amixer set Master 2%+")
+--         , ("M-C-e",	spawn "amixer set Master 2%-")
+--         , ("M-C-u",	spawn "amixer set Master 2%+")
          -- CycleWS
-         , ("M-s",	nextWS)
-         , ("M-n",	prevWS)
+--         , ("M-s",	nextWS)
+--         , ("M-n",	prevWS)
 	 -- Swap Workspaces
-	 , ("M-S-s",	swapTo Next)
-	 , ("M-S-n",	swapTo Prev)
+--	     , ("M-S-s",	swapTo Next)
+--	     , ("M-S-n",	swapTo Prev)
+--         , ((myAltMask, xK_space), viewEmptyWorkspace)
          -- Prompt
-         , ("M-C-p",	runOrRaisePrompt defaultXPConfig)
-         , ("M-C-.",	shellPrompt defaultXPConfig)
-         , ("M-C-j",	jumpPrompt)
+--         , ("M-C-p",	runOrRaisePrompt defaultXPConfig)
+--         , ("M-C-.",	shellPrompt defaultXPConfig)
+--         , ("M-C-j",	jumpPrompt)
 	 -- Toggle Struts
-	 , ("M-S-s",	sendMessage ToggleStruts)
-	 -- Topics
-	 , ("M-a",	currentTopicAction myTopicConfig)
-	 , ("M-g",	promptedGoto)
-	 , ("M-c",	promptedGotoOtherScreen)
-	 , ("M-S-g",	promptedShift)
+--	     , ("M-S-s",	sendMessage ToggleStruts)
+--	 -- Topics
+--	     , ("M-a",	currentTopicAction myTopicConfig)
+---	     , ("M-g",	promptedGoto)
+--	     , ("M-c",	promptedGotoOtherScreen)
+--	     , ("M-S-g",	promptedShift)
          -- Restart
-         , ("M-q",	spawn myRestart)
-	 ]
-        ++	
+--         , ("M-q",	spawn myRestart)
+--	 ]
+--        ++	
          -- Change Xinerama bindings
-         [ ("M-"++key, screenWorkspace sc >>= flip whenJust (windows . f))
-             | (key, sc) <- zip ["w", "v", "z"] [0..]
-             , (f, m) <- [(W.view, 0)]] 
+--         [ ("M-"++key, screenWorkspace sc >>= flip whenJust (windows . f))
+--             | (key, sc) <- zip ["w", "v", "z"] [0..]
+--             , (f, m) <- [(S.view, 0)]] 
  
 spawnShell = currentTopicDir myTopicConfig >>= spawnShellIn
  
@@ -202,7 +284,7 @@ promptedGotoOtherScreen =
 		goto ws
  
 promptedShift :: X ()
-promptedShift = workspacePrompt myXPConfig $ windows . W.shift
+promptedShift = workspacePrompt myXPConfig $ windows . S.shift
  
 jumpPrompt :: X ()
 jumpPrompt = inputPrompt defaultXPConfig ("Jump") ?+ spawnJump
@@ -213,9 +295,9 @@ spawnJump s = spawn ("nyxmms2 jump artist:" ++ s)
 -- Helper functions
 --
 -- Avoid changing master on new window creation
-avoidMaster :: W.StackSet i l a s sd -> W.StackSet i l a s sd
-avoidMaster = W.modify' $ \c -> case c of
-	W.Stack t [] (r:rs) -> W.Stack t [r] rs
+avoidMaster :: S.StackSet i l a s sd -> S.StackSet i l a s sd
+avoidMaster = S.modify' $ \c -> case c of
+	S.Stack t [] (r:rs) -> S.Stack t [r] rs
 	otherwise			-> c
  
 -- Kill zombie dzens before normal xmonad restart
