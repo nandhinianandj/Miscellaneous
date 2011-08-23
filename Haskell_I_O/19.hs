e39bc65e9abb504a543da267bfdc6dd399b10f12
Software Mechanic <softwaremechanic32@gmail.com>
2011-08-23 22:35:13 +0530

    Fixes to get random quote. just a couple more and it would be ready

--- Haskell_I_O/19.hs
+++ Haskell_I_O/19.hs
@@ -1,6 +1,7 @@
 import System.IO
 import System.Directory
 import Data.List
+import System.Random
 
 withFile' :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
 withFile' path mode f = do
@@ -9,17 +10,22 @@ withFile' path mode f = do
   hClose handle
   return result
 
-main = do
-  handle <- openFile "../Quotes.txt" ReadMode
-  handle <- openFile "/home/anand/.signature" WriteMode
+pickRandom :: Int -> IO Int
+pickRandom x = getStdRandom (randomR (1,x))
 
-  contents <- hGetContents handle
+main = do
+  quotes_handle <- openFile "../Quotes.txt" ReadMode
+  sign_handle <- openFile "/home/anand/.signature" WriteMode
+
+  contents <- hGetContents quotes_handle
   let quotes = lines contents
       numberedQuotes = zipWith (\n line -> show n ++ "-" ++ line) [0..] quotes
       no_of_quotes = length quotes
+      rand_no = pickRandom no_of_quotes
 
-  putStr $ unlines quotes
-  hClose handle
+  putStr $ show (head (drop (rand_no-1) (unlines quotes)))
+  hClose quotes_handle
+  hClose sign_handle
 
 
 
--- Haskell_I_O/rand_quote.hs
+++ Haskell_I_O/rand_quote.hs
@@ -2,6 +2,10 @@ import System.IO
 import System.Random
 
 getLines = liftM lines . readFile
+
+pickRandom :: x -> IO Int
+pickRandom x = getStdRandom (randomR (1,x))
+
 main = do
   contents <- readFile "/home/anand/workspace/my_projects/Miscellaneous/Quotes.txt"
   mapM_ putStrLn contents
