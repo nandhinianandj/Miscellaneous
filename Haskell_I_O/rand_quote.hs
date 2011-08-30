386e811eae81acc881791064475816cc1cab7b03
Software Mechanic <softwaremechanic32@gmail.com>
2011-08-31 00:32:38 +0530

    Finally, learnt to extract a random string from a list of string.

--- Haskell_I_O/rand_quote.hs
+++ Haskell_I_O/rand_quote.hs
@@ -1,11 +1,14 @@
 import System.IO
 import System.Random
 
-getLines = liftM lines . readFile
+--getLines = liftM lines . readFile
 
-pickRandom :: x -> IO Int
-pickRandom x = getStdRandom (randomR (1,x))
+--pickRandom :: (Num (([a]->Int) ->[String] ->(Int,Int)))
+pickRandom x = randomRIO (0,length x) >>= return .(x !!)
 
+--getRandomQuote str = pickRandom (str)
 main = do
   contents <- readFile "/home/anand/workspace/my_projects/Miscellaneous/Quotes.txt"
-  mapM_ putStrLn contents
+  let quotes = lines contents
+  let sign = pickRandom quotes
+  sign
