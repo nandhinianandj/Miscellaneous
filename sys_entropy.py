3571cca21ccff4ef763b39be84c69b6eb8d85711
Software Mechanic <softwaremechanic32@gmail.com>
2011-09-24 22:38:09 +0530

    Fixes to get the date,uptime,available entropy functions running.

--- sys_entropy.py
+++ sys_entropy.py
@@ -1,25 +1,33 @@
 #!/bin/env python
 import sys, os
 import subprocess
+import timeit
+import datetime
 
 log_file = '/home/anand/.sys_entropy.log'
 
 def main():
     out_fd = open(log_file,'a')
-    date_proc = subprocess.Popen('date')
-    date = date_proc.communicate()
-    out_fd.write("Date: %s",date)
-    uptime_proc = subprocess.Popen('uptime')
-    uptime = uptime_proc.communicate()
-    out_fd.write("Uptime:%s",uptime)
+    date = datetime.datetime.utcnow()
+    out_fd.write("Date: %s\n"%date)
+
+    uptime_proc = subprocess.Popen('uptime',stdout=out_fd)
 
     #Available entropy from the /proc fs
     proc_fd = open('/proc/sys/kernel/random/entropy_avail')
     avail_entropy = int(proc_fd.read().strip('\n'))
     proc_fd.close()
-    out_fd.write("Available entropy value(from procfs):%s",avail_entropy)
+    out_fd.write("\nAvailable entropy value(from procfs):%s\n"%avail_entropy)
+
+    t = timeit.Timer(number=1)
+    t.timeit(os_system_dd,number=1)
+    os.remove('sys_entropy_random')
+    out_fd.close()
+
+def os_system_dd():
+    subprocess.call('dd','if=/dev/random of=sys_entropy_random bs = 1M count = 500')
 
 
-
-if __name__ == '__prog__':
+if __name__ == '__main__':
+    print __name__
     main()
