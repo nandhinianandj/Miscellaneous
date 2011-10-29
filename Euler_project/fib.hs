3bf9b4471ef72daa3cb667019d8ccdcdfae220e5
Software Mechanic <softwaremechanic32@gmail.com>
2011-10-29 21:36:35 +0530

    More minor changes...

--- Euler_project/fib.hs
+++ Euler_project/fib.hs
@@ -11,11 +11,12 @@ fib_naive 1 = 1
 fib_naive n = fib_naive(n-1) + fib_naive(n-2)
 
 --fib_infinite ::
-fib_infinite n = fib_infinite!!n
+--fib_infinite n = fib_infinite!!n
 
+fib_scan = 0:1:zipWith (+) fib_scan (tail fib_scan)
 
 --euler_prob_2 :: Int -> Int
-euler_prob_2 n = sum (takeWhile (\x -> x < n) (fib_infinite ))
+euler_prob_2 n = sum (takeWhile (\x -> x < n) (fib_scan ))
 
 --module Main where
 --main :: IO()
