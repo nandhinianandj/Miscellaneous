2d5fe9f54dea480ac9a8510ef451f3070480be90
Software Mechanic <softwaremechanic32@gmail.com>
2012-02-16 23:27:59 +0530

    Just adding all leftovers

--- Euler_project/fib.hs
+++ Euler_project/fib.hs
@@ -16,7 +16,7 @@ fib_naive n = fib_naive(n-1) + fib_naive(n-2)
 fib_scan = 0:1:zipWith (+) fib_scan (tail fib_scan)
 
 --euler_prob_2 :: Int -> Int
-euler_prob_2 n = sum (takeWhile (\x -> x < n) (fib_scan ))
+euler_prob_2 n = sum (takeWhile (\x -> (even x && x < n)) (fib_scan ))
 
 --module Main where
 --main :: IO()
