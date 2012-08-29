7fb869a2dc14d5db1e578e56d635b22be152003f
Software Mechanic <softwaremechanic32@gmail.com>
2012-08-30 00:43:11 +0530

    Just some more exploring and experimentations

---
+++ C/ip_addr_to_num.c
@@ -0,0 +1,23 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+  struct in_addr addr;
+  if (argc != 2) {
+    fprintf(stderr, "%s <dotted-address>\n", argv[0]);
+    exit(EXIT_FAILURE);
+              }
+
+   if (inet_aton(argv[1], &addr) == 0) {
+           perror("inet_aton");
+           exit(EXIT_FAILURE);
+             }
+
+    printf("address: %l\n",addr);
+    printf("%s\n", inet_ntoa(addr));
+    exit(EXIT_SUCCESS);
+}
+
