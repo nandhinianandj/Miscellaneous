affc6b46ebaa8678a1b4bc685e2223081229d1ea
Software Mechanic <softwaremechanic32@gmail.com>
2012-11-20 06:08:07 +0530

    more changes to take dynamic input.. guess what segfault.. yay.. me..

--- functional_c/linked_list.c
+++ functional_c/linked_list.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#Immutable note the consts.
+//Immutable note the consts.
 typedef struct IntList_s const * const IntList;
 static IntList const Nil = NULL;
 
@@ -49,21 +49,6 @@ void void_map_array( VoidMappable const f
 
 void print_value(int32_t const value);
 
-int main(int argc, char * argv[])
-{
-  (void)argc;
-  (void)argv;
-  int32_t my_array[] = {2,5,6,1,9,23,7653,12,0,4};
-  uint32_t const my_array_size = sizeof(my_array)/sizeof(my_array[0]);
-
-  int32_t result[my_array_size];
-  make_list(my_array_size,my_array,Nil, reverse_and_stuff,result);
-
-  void_map_array(print_value, my_array_size, result);
-  printf("\n");
-
-  return 0;
-}
 
 
 void make_list( uint32_t const array_size
@@ -130,3 +115,33 @@ void void_map_array(VoidMappable const f
   }
 }
 
+void print_value(int32_t const value)
+{
+  printf("%d ", value);
+}
+
+
+
+int main(int argc, char * argv[])
+{
+  (void)argc;
+
+  (void)argv;
+
+//  int32_t my_array[] = {2,5,6,1,9,23,7653,12,0,4};
+  int32_t my_array[] = {}; // = argv;
+
+  for (int i=0;i<argc;++i)
+  {
+    my_array[i] = atoi(argv[i]);
+  }
+  uint32_t const my_array_size = sizeof(my_array)/sizeof(my_array[0]);
+
+  int32_t result[my_array_size];
+  make_list(my_array_size,my_array,Nil, reverse_and_stuff,result);
+
+  void_map_array(print_value, my_array_size, result);
+  printf("\n");
+
+  return 0;
+}
