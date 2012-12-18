25f7ee3ea940ae54b246f010121f4681d7da7de4
Anand Jeyahar <Anand.jeyahar@gmail.com>
2012-12-18 23:38:22 +0530

    more changes

--- theendeavour_code_snippets/digits_in_powers_of_2.py
+++ theendeavour_code_snippets/digits_in_powers_of_2.py
@@ -5,14 +5,20 @@ def digits(n):
         n /= 10
     return s
 
+def seven_in_digits_str(n):
+    s = str(n)
+    if '7' not in s:
+        return "No 7, %s",s
+    else:
+        return False
 
 def main(n,m):
     for i in range(n, m):
         p = 2**i
-        if 7 not in digits(p):
+        if seven_in_digits_str(p):
             print i, p
 
 if __name__ == '__main__':
     import sys
-    n,m = int(sys.argv[1]),int(sys.argv[2])
+    n,m = long(sys.argv[1]),long(sys.argv[2])
     main(n,m)
