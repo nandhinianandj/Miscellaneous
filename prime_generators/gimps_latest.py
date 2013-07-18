cd857830ac9c36934058e050ed0952c883476edb
Anand Jeyahar <Anand.jeyahar@gmail.com>
2013-07-18 19:55:41 +0530

    adding a set of prime number generators..

---
+++ prime_generators/gimps_latest.py
@@ -0,0 +1,1 @@
+latest_prime = 2**57885161 -1  #48 th mersenne prime
---
+++ prime_generators/mills_const.py
@@ -0,0 +1,9 @@
+
+
+long long theta = 1.3063778838630806904686144926026  #Mill's Constant.
+
+def get_prime(n):
+    return theta**(3**n)
+
+
+cdef longdouble theta = 1.3063778838630806904686144926026  #Mill's Constant.
