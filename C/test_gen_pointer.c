a4a7e017676f183495d2b08b96d3f2e3e7d8d70c
Software Mechanic <softwaremechanic32@gmail.com>
2013-10-21 19:41:10 +0530

    Test_gen_pointer testing for redis contrib.

---
+++ C/test_gen_pointer.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+typedef struct testA
+{
+    int a;
+    float b;
+    char *c;
+}test;
+
+void main()
+{
+    test *A,*B;
+
+    A->a = 5;
+    A->b= 12.0;
+    A->c = "eue";
+
+    B = malloc(sizeof(test));
+    B = (void*) A;
+
+    printf("%d\t %f \t %s \n ",B->a,B->b,B->c);
+    free(B);
+}
