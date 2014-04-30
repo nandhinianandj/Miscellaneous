5311c9a2444440069cf846c6bdc791271b87af5b
Software Mechanic <softwaremechanic32@gmail.com>
2014-05-01 00:17:33 +0530

    more additions.

--- Haskell/euclid.hs
+++ Haskell/euclid.hs
@@ -1,7 +1,10 @@
 import Data.List
 data Int = -2147483648 | -2147483647 | .... | -1 | 0 |1 | 2 | ... | 2147483647
 data Point = Int Int
-data Line = Point Point
-data Lines = take Line
+data LineSegment = Point Point
+data Lines = take Point
 data Degree = 0 | 1 | 2 | ... | 360
 data Circle = Point Line
+
+-- Add restriced line segment for capturing the circle constraint
+-- Add constraint for right-angle/90 degrees
