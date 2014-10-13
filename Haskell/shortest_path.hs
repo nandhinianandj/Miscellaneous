d1f1597e05765c0413f9da8fe5b53dce41b696ee
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-13 14:18:43 +0530

    omue

--- Haskell/shortest_path.hs
+++ Haskell/shortest_path.hs
@@ -1,3 +1,4 @@
+import Data.List
 -- data Node = Node Road Road | EndNode Road
 -- data Road = Road Int Node
 -- data Node = Node Road (Maybe Road)
@@ -6,7 +7,7 @@ data Section = Section {getA :: Int, getB :: Int, getC :: Int } deriving (Show)
 type RoadSystem = [Section]
 
 heathrowToLondon :: RoadSystem
-heathrowToLondo = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
+heathrowToLondon = [Section 50 10 30, Section 5 90 20, Section 40 2 25, Section 10 8 0]
 
 data Label = A | B |C deriving (Show)
 type Path = [(Label, Int)]
@@ -21,9 +22,32 @@ roadStep (pathA, pathB) (Section a b c) =
         crossPriceToB = priceA + a + c
         newPathToA = if forwardPriceToA <= crossPriceToA
                         then (A,a):pathA
-                        else (C,c):(B:b):pathB
+                        else (C,c):pathB
         newPathToB = if forwardPriceToB <= crossPriceToB
                         then (B,b):pathB
-                        else (C,c):(A:a):pathA
+                        else (C,c):pathA
+    in (newPathToA, newPathToB)
+
+optimalPath :: RoadSystem -> Path
+optimalPath roadSystem =
+    let (bestAPath, bestBPath) = foldl roadStep([], []) roadSystem
+    in if sum (map snd bestAPath) <= sum(map snd bestBPath)
+        then reverse bestAPath
+        else reverse bestBPath
+
+groupsOf :: Int -> [a] -> [[a]]
+groupsOf 0 _ = undefined
+groupsOf _ [] = []
+groupsOf n xs = take n xs : groupsOf n (drop n xs)
 
 
+main = do
+    contents <- getContents
+    let threes = groupsOf 3 (map read $ lines contents)
+        roadSystem = map (\[a,b,c] -> Section a b c) threes
+        path = optimalPath roadSystem
+        pathString = concat $ map (show . fst) path
+        pathPrice = sum $ map snd path
+    putStrLn $ "The best path to take is: " ++ pathString
+    putStrLn $ "The price is : " ++ show pathPrice
+-- main = optimalPath heathrowToLondon
