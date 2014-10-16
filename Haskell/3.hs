c03db3c804f4d1483d2d73750891cace1e902f55
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-16 16:56:24 +0530

    just cleaning up.

--- Haskell/3.hs
+++
@@ -1,4 +0,0 @@
-main = do
-  putStrLn "Hello, what's your name?"
-  name <- getLine
-  putStrLn $ "Read this carefully, because this is your future:" ++ tellFortune name
--- Haskell/functors.hs
+++ Haskell/functors.hs
@@ -8,9 +8,20 @@
 --         line <- fmap reverse getLine
 --         putStrLn $ "You said " ++ line ++ " backwards!"
 --         putStrLn $ "Yes, you said" ++ line ++ "backwards"
-import Data.Char
-import Data.List
+--import Data.Char
+--import Data.List
+--
+--main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine
+--          putStrLn line
+--data Test a = Test a
+--
+--instance Functor Test where
+--    fmap f (Just x) = Just (f x)
+--    fmap f Nothing = Nothing
+--
 
-main = do line <- fmap (intersperse '-' . reverse . map toUpper) getLine
-          putStrLn line
+data CMaybe a = CNothing | CJust Int a deriving (Show)
 
+instance Functor CMaybe where
+    fmap f CNothing = CNothing
+    fmap f (CJust counter x) = CJust (counter+1) (f x)
