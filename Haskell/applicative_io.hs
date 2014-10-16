fc61981cd2bfa7098377d6c06c02748a7b3da9d3
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-16 21:29:15 +0530

    ou

--- Haskell/applicative_io.hs
+++ Haskell/applicative_io.hs
@@ -7,4 +7,8 @@ import Control.Applicative
 myAction :: IO String
 myAction = (++) <$> getLine <*> getLine
 
+sequenceA :: (Applicative f) => [f a] -> f [a]
+sequenceA [] = pure []
+sequenceA (x:xs) = (:) <$> X <*> sequenceA xs
+
 main = myAction
