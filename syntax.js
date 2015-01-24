a57d605c1e9914ae9868b7c0b199399813edb13f
Software Mechanic <softwaremechanic32@gmail.com>
2015-01-24 11:02:29 +0530

    more syntax

--- syntax.js
+++ syntax.js
@@ -116,3 +116,15 @@ map(array,
         }
 
 }, finalCbMap);
+
+// Infinite fibonacci generator
+// Generators
+//
+function* fibGen () {
+  var current = 0, next = 1, swap
+  while (true) {
+    swap = current, current = next
+    next = swap + next
+    yield current
+  }
+}
