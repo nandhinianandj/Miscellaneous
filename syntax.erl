6dfcf0d5b02ed493d33ff9f2bf8d67fe6e4dd34e
Software Mechanic <softwaremechanic32@gmail.com>
2015-04-25 23:20:32 +0530

    restructuring.

---
+++ syntax.erl
@@ -0,0 +1,13 @@
+%Variables assignment
+Var = 24. % Note the first letter capital and termination char .
+%Pattern Matching
+Person = {person, {name, "Agent Smith"}, {profession, "killing programs}}.
+
+%pattern match
+{person, {name, Name}, {profession, Profession}} = Person.
+
+Name.
+
+
+
+
