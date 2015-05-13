df4e8d17b9e0a094852b418cb1cf7d74ff119b3e
Software Mechanic <softwaremechanic32@gmail.com>
2015-05-13 22:18:19 +0530

    erlang...

--- syntax.erl
+++ syntax.erl
@@ -26,8 +26,37 @@ if
         success;
     ProgramsTerminated < 0 ->
         error;
-    true -> zero
+    true -> zero    % for default values/else..if is a function here.
 end.
+
+
+
+% Anonymous functions
+Negate = fun(I) -> -I end.
+
+Numbers = [1,2,3,4].
+lists:foreach(fun(Number) -> io:format("~p~n", [Number]) end, Numbers).
+
+lists:map(fun(X) -> X +1 end, Numbers).
+
+Small = fun(X) -> X < 3 end.
+
+
+lists:filter(Small, Numbers).
+
+lists:all(Small, [0,1,2]).
+lists:all(Small, Numbers).
+
+lists:any(Small, [0,1,2,3]).
+lists:any(Small, [3,4,5]).
+
+lists:takewhile(Small, Numbers).
+lists:dropwhile(Small, Numbers).
+
+lists:foldl(fun(X, Sum) -> X + Sum end, 0, Numbers).
+
+Adder = fun(ListItem, SumSoFar) -> ListItem + SumSoFar end.
+
 
 
 
