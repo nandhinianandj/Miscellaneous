898f9e7d549c79cb38045c021db4a631fced07ce
Anand Jeyahar <anand.jeyahar@gmail.com>
2015-07-16 10:45:21 +0530

    new philosopher project

---
+++ rustc/philosopher/src/lib.rs
@@ -0,0 +1,50 @@
+#[test]
+use std::thread;
+use std::sync::Mutex;
+
+struct Table {
+    forks: Vec<Mutex<()>>,
+}
+struct Philosopher {
+    name: String,
+    }
+
+impl Philosopher {
+    fn new(name: &str) -> Philosopher {
+        Philosopher {
+            name: name.to_string(),
+            }
+    }
+    fn eat(&self) {
+        println!("{} is eating", self.name);
+        thread::sleep_ms(1000);
+        println!("{} is done eating", self.name);
+    }
+}
+
+fn main() {
+    let philosophers = vec![
+            Philosopher::new("Baruch Spinoza"),
+            Philosopher::new("Giles Deleuza"),
+            Philosopher::new("Karl Marx"),
+            Philosopher::new("Friedrich Nietzsche"),
+            Philosopher::new("Michel Foucault"),
+    ];
+
+    let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        thread::spawn(move || {
+            p.eat();
+        })
+    }).collect();
+
+    for h in handles {
+        h.join().unwrap();
+    }
+
+    for p in &philosophers {
+        p.eat();
+    }
+}
+
+
+
---
+++ rustc/philosopher/target/debug/philosopher.d
@@ -0,0 +1,4 @@
+/home/anand/playspace/github_stuff_public/softwaremechanic/Miscellaneous/rustc/philosopher/target/debug/libphilosopher.rlib: src/lib.rs
+
+/home/anand/playspace/github_stuff_public/softwaremechanic/Miscellaneous/rustc/philosopher/target/debug/philosopher.d: src/lib.rs
+
