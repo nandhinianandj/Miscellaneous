33629c80bbc73a5a5a2c6842caaefdbc2970cc36
Anand Jeyahar <anand.jeyahar@gmail.com>
2015-07-21 23:10:22 +0530

    more changes to rustc philosopher problem.

--- rustc/philosopher/src/lib.rs
+++ rustc/philosopher/src/lib.rs
@@ -1,21 +1,27 @@
 #[test]
 use std::thread;
-use std::sync::Mutex;
+use std::sync::{Mutex, Arc};
 
 struct Table {
     forks: Vec<Mutex<()>>,
 }
 struct Philosopher {
     name: String,
+    left: usize,
+    right:usize,
     }
 
 impl Philosopher {
-    fn new(name: &str) -> Philosopher {
+    fn new(name: &str, left: usize, right: usize) -> Philosopher {
         Philosopher {
             name: name.to_string(),
+            left: left,
+            right:right,
             }
     }
-    fn eat(&self) {
+    fn eat(&self, table: &Table) {
+        let _left = table.forks[self.left].lock().unwrap();
+        let _right= table.forks[self.right].lock().unwrap();
         println!("{} is eating", self.name);
         thread::sleep_ms(1000);
         println!("{} is done eating", self.name);
@@ -23,17 +29,26 @@ impl Philosopher {
 }
 
 fn main() {
+    let table = Arc::new(Table { forks: vec![
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+            Mutex::new(()),
+             ]});
+
     let philosophers = vec![
-            Philosopher::new("Baruch Spinoza"),
-            Philosopher::new("Giles Deleuza"),
-            Philosopher::new("Karl Marx"),
-            Philosopher::new("Friedrich Nietzsche"),
-            Philosopher::new("Michel Foucault"),
+            Philosopher::new("Baruch Spinoza", 0, 1),
+            Philosopher::new("Giles Deleuza", 1, 2),
+            Philosopher::new("Karl Marx", 2,3),
+            Philosopher::new("Friedrich Nietzsche", 3,4),
+            Philosopher::new("Michel Foucault", 0,4),
     ];
 
     let handles: Vec<_> = philosophers.into_iter().map(|p| {
+        let table = table.clone();
         thread::spawn(move || {
-            p.eat();
+            p.eat(&table);
         })
     }).collect();
 
@@ -41,9 +56,6 @@ fn main() {
         h.join().unwrap();
     }
 
-    for p in &philosophers {
-        p.eat();
-    }
 }
 
 
--- rustc/philosopher/target/debug/philosopher.d
+++ rustc/philosopher/target/debug/philosopher.d
@@ -1,4 +1,4 @@
-/home/anand/playspace/github_stuff_public/softwaremechanic/Miscellaneous/rustc/philosopher/target/debug/libphilosopher.rlib: src/lib.rs
+/home/anand/playspace/Miscellaneous/rustc/philosopher/target/debug/libphilosopher.rlib: src/lib.rs
 
-/home/anand/playspace/github_stuff_public/softwaremechanic/Miscellaneous/rustc/philosopher/target/debug/philosopher.d: src/lib.rs
+/home/anand/playspace/Miscellaneous/rustc/philosopher/target/debug/philosopher.d: src/lib.rs
 
