f7ac83d5ececd51c31b4787a00fe6757e4ff7c95
Anand Jeyahar <anand.jeyahar@gmail.com>
2015-07-21 22:35:52 +0530

    more stuff

---
+++ Python/data_analysis/outliers.py
@@ -0,0 +1,32 @@
+
+################################################################
+#   UniVariate Outliers
+##################################################################
+
+def 3sigmaDeviation(seq, threshold=3, passes=1):
+    # filter and remove values beyond +/- 3 sigma variance
+    # default one pass.
+    pass
+
+def interQuartileRangeDev(seq, threshold=1.5):
+    # filter values beyound the +/- 1.5 quartile range
+    pass
+
+def capPercentile(seq, threshold=5):
+    # filter values from the 5th and 95th percentile range
+    pass
+
+
+################################################################
+#   Multi/Bi-Variate Outliers
+##################################################################
+#   -- Measured using an index of influence or leverage or distance
+#   -- Popular indices such as Mahalanobis’ distance and Cook’s D are frequently used .
+#   --  statistical measure like STUDENT, COOKD, RSTUDENT and others.
+
+###
+# Removal methods
+#    -- Deleting
+#    -- Transforming(like log or others) and binning
+#    -- Imputing values
+
