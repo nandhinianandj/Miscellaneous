55a90b581f57e16a999d8bc118b73540125c02d6
Anand Jeyahar <anand.jeyahar@gmail.com>
2015-08-20 00:29:22 +0530

    more stuff

---
+++ Python/imageSearch.py
@@ -0,0 +1,3 @@
+# Call opencv sift algorithm to identify keypoints(see file in the same folder)
+# Draw a region around the image with curve algorithm from points(bezier curve??)
+# take that region and create a classifier(logistic/k-means etc..) and compare them with each frame in the video
