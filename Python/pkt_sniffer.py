e58dae62a802d6f233727d68d9a2145ce6b5a11e
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-06-06 16:43:27 +0530

    more  stuff.

--- Python/pkt_sniffer.py
+++ Python/pkt_sniffer.py
@@ -10,4 +10,18 @@ def PacketHandler(pkt):
                 ap_list.append(pkt.addr2)
                 print "AP MAC: %s with SSID : %s" %(pkt.addr2,pkt.info)
 
+def raw_socket_ssid_sniffer():
+	import socket
+	rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+	rawSocket.bind(("mon0", 0x0003))
+	ap_list = set()
+	while True :
+		pkt = rawSocket.recvfrom(2048)[0]
+		if pkt[26] == "\x80" :
+			if pkt[36:42] not in ap_list  and ord(pkt[63]) > 0:
+				ap_list.add(pkt[36:42])
+				print "SSID: %s  AP MAC: %s" % (pkt[64:64 +ord(pkt[63])], pkt[36:42].encode('hex'))
+
 sniff(iface="mon0",prn=PacketHandler)
+
+#raw_socket_ssid_sniffer()
