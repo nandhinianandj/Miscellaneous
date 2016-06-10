3aaef259203cc07d04811145d5737f65fc90e6a6
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-06-10 21:00:37 +0530

    more sutff from john D cook

---
+++ Haskell/statsMoments.hs
@@ -0,0 +1,13 @@
+moments (n, m1, m2, m3, m4) x = (n', m1', m2', m3', m4')
+        where
+            n' = n + 1
+            delta = x - m1
+            delta_n = delta / n'
+            delta_n2 = delta_n**2
+            t = delta*delta_n*n
+            m1' = m1 + delta_n
+            m4' = m4 + t*delta_n2*(n'*n' - 3*n' + 3) + 6*delta_n2*m2 - 4*delta_n*m3
+            m3' = m3 + t*delta_n*(n' - 2) - 3*delta_n*m2
+            m2' = m2 + t
+
+mvsk (n, m1, m2, m3, m4) = (m1, m2/(n-1.0), (sqrt n)*m3/m2**1.5, n*m4/m2**2 - 3.0)
