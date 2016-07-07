a747fd69b74a1a924ea2b8cf20821172c5ad4c01
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-07-07 20:23:22 +0530

    basic logic .. but might not perform well..

--- practice/skiiing-in-singapore/first_cut.py
+++ practice/skiiing-in-singapore/first_cut.py
@@ -1,6 +1,5 @@
-import networkx
+import networkx as nx
 import numpy as np
-terrain_map = np.loadtxt('sample1.txt')
 
 # This is a graph search problem with 4 edges coming out of all vertices(well except the corner
 # ones)
@@ -29,6 +28,36 @@ def starting_positions(nparray, threshold=0.5):
 # Hmm. may be that heurisitic is wrong.
 def prob_matrix(size=(1000,1000)):
     mat = np.zeros(size)
-    for
+
+def main(filename='sample1.txt'):
+    terrain_altitude_map = np.loadtxt(filename)
+    G = nx.Graph()
+    # Construct a graph with nodes as a triple (x,y, prev_node)
+    for(x,y), value in np.ndenumerate(terrain_altitude_map):
+        G.add_node((x,y))
+
+        # outcoming ski-edge so curr_altitude - new_altitude = height difference
+        # +1 for all edge weights for the objective function to include length of the path
+        if x+1 < terrain_altitude_map.shape[0]:
+            # Instead of checking for lower height just using the negative value, for now
+            G.add_edge((x,y),(x+1, y),{'weight': -1 * (value - terrain_altitude_map[x+1][y] +1) })
+
+        if y+1 < terrain_altitude_map.shape[1]:
+            G.add_edge((x,y),(x, y+1), {'weight':-1 * (value - terrain_altitude_map[x][y+1] + 1) })
+
+        # incoming ski-edge so new_altitude - curr_altitude = height difference
+        if x-1 > 0:
+            G.add_edge((x,y),(x-1, y), {'weight': -1 * (terrain_altitude_map[x-1][y] - value + 1)})
+        if y-1 > 0:
+            G.add_edge((x,y),(x, y-1), {'weight': -1 * (terrain_altitude_map[x][y-1] - value + 1)})
+
+    ans = nx.minimum_spanning_tree(G,'weight')
+    for edge in ans.edges():
+        print(terrain_altitude_map[edge[0][0]][edge[0][1]] + '-')
+        print(terrain_altitude_map[edge[1][0]][edge[1][1]] + '-')
 
 
+
+
+if __name__ == '__main__':
+    main()
