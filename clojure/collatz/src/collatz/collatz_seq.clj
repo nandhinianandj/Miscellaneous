2a057c18c0c19d7296dcaa240c856078eb300edd
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-08-22 19:03:56 +0530

    more stuff

--- clojure/collatz/src/collatz/collatz_seq.clj
+++ clojure/collatz/src/collatz/collatz_seq.clj
@@ -8,7 +8,7 @@
 ;               b, Any number that is a power of 2 will have seq. length of power + 1. for ex: 2^2 = 4
 ;will have seq length 3.  So once we hit a 2^n form we can stop and find log 2 + 1, instead of
 ;generating sequence
-
+(ns example.core (:require [clojure.math.numeric-tower :as math]))
 (defn power_of_2 [n] (and (not= n 0) (== (bit-and n (- n 1) ) 0) ) )
-(defn collatz [ln] (defn n1 [ln] first ln) (defn n2 [[_ second]] second)
+(defn collatz [ln] (if (==  ln 1) 1 if (== (% n 2) 0) if (power_of_2 n) (+ 1
   )
