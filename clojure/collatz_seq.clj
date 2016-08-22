73bec93ca691be0e9a808b65cea03ad77ccb0f12
Anand Jeyahar <anand.jeyahar@gmail.com>
2016-08-22 17:46:09 +0530

    first commit.. dummy clojure file..

---
+++ clojure/collatz_seq.clj
@@ -0,0 +1,12 @@
+; -- program to a, find decidability of a collatz seq.
+; --            b, find length of a collatz seq
+; --        given: an integer
+
+; -- Insights/key shortcuts:
+;               a, each of the element in a sequence will have had it's collatz seq length already
+;calculated
+;               b, Any number that is a power of 2 will have seq. length of power + 1. for ex: 2^2 = 4
+;will have seq length 3.  So once we hit a 2^n form we can stop and find log 2 + 1, instead of
+;generating sequence
+
+()
