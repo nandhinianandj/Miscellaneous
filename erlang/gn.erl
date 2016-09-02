e1e1174d1d6c7e059385ce85032b8b51f80b833d
Software Mechanic <softwaremechanic32@gmail.com>
2016-09-02 14:22:42 +0530

    more testing for erlang distributed bot.

---
+++ erlang/gn.erl
@@ -0,0 +1,27 @@
+-module(gn).
+-export([universal_server/0,
+	 factorial_server/0,
+	 test/0]).
+universal_server() ->
+    receive
+       {become, F} ->
+           F()
+    end.
+
+factorial_server() ->
+    receive
+       {From, N} ->
+           From ! factorial(N),
+           factorial_server()
+    end.
+
+factorial(0) -> 1;
+factorial(N) -> N * factorial(N-1).
+
+test() ->
+    Pid = spawn(fun universal_server/0),
+    Pid ! {become, fun factorial_server/0},
+    Pid ! {self(), 50},
+    receive
+        X -> X
+    end.
