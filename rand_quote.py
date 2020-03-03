be574e2e75cf79ae0a1d72749e765cd0fb2f0d82
Nandhini Anand <cattykatrina4@gmail.com>
2020-03-03 21:24:47 +0530

    more fixes

--- rand_quote.py
+++ rand_quote.py
@@ -4,8 +4,8 @@ import sys
 
 
 def main():
-#    quote_file = sys.argv[1]
-    quote_file = ''
+    quote_file = sys.argv[1]
+    #quote_file = '/home/nandhini/my_writings/wordpress_blog_posts/aangjie/quotes.md'
     quote_fd = open(quote_file)
     quote_lines = quote_fd.read()
     quotes = quote_lines.split("~Aang Jie")
