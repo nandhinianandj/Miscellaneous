e0c6ddb2358b37eb01c211158b2ef2a2f3a2cd67
Yaay Nands <16502598+nandhinianandj@users.noreply.github.com>
2024-03-12 01:17:01 +0530

    Decision making is the science and/or art of navigating through the space of problem sets.
    --Aang Jie

--- rand_quote.py
+++ rand_quote.py
@@ -1,16 +1,15 @@
 import random
 import sys
-
-
+import os
 
 def main():
-    quote_file = sys.argv[1]
-    #quote_file = '/home/nandhini/my_writings/wordpress_blog_posts/aangjie/quotes.md'
+    quote_file = os.path.join(os.getenv("HOME") , 'my_writings/wordpress_blog_posts/aangjie/quotes.md')
+    quote_file = sys.argv[1]  if len(sys.argv) > 1 else quote_file
     quote_fd = open(quote_file)
     quote_lines = quote_fd.read()
     quotes = quote_lines.split("~Aang Jie")
     quote_count = random.choice(range(len(quotes)))
-    print quotes[quote_count]
+    print(quotes[quote_count])
 
 if __name__ == '__main__':
     main()
